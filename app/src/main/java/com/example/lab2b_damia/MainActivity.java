6a9128d73d366b00009817d41cf8123a83878c66
dmiasyfqh <2022645986@student.uitm.edu.my>
2023-11-22 20:10:53 +0800

    my first commit

---
+++ app/src/main/java/com/example/lab2b_damia/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.lab2b_damia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        String[] values = new String[]{
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+    }
+}
